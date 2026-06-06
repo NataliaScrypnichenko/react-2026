ef7a5184cb9747382fab9ad9306721529f5b94ef
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-06-06 21:34:21 +0300

    виконала

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
-
-
+ const coursesTitleArray:string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 
 
 const App= ()=> {
 
   return (
-    <>
-       <ul>
+  <>
+      <ul>
+          {
+              coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)
+          }
+      </ul>
 
-       </ul>
-    </>
+  </>
   )
 }
 
