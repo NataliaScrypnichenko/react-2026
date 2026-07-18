2d5c1d38d5959436ac09730f35dee848ed06b758
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-07-18 22:44:05 +0300

    HW1/task3-еще нужно работать

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {simpsons} from "./data/simpsons.ts";
+import CharacterComponent from "./components/CharacterComponent/CharacterComponent.tsx";
+
+
+
+
 
 function App() {
 
 
   return (
     <>
-
+      {
+        simpsons.map((simpson,index)=><CharacterComponent key={index} simpson={simpson}/>)
+      }
     </>
   )
 }
