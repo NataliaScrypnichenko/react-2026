7f0321e45dac3d3923e45c882ac25e181d1dc9de
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-08-02 21:47:34 +0300

    I need to made again #x7sQQ3cJ3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import './App.css'
+import {useEffect, useState} from "react";
 
-function App() {
+const App = () => {
+
+  const [posts,setPosts]= useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(value =>
+        setPosts(value))
+
+    return () => {}
+
+  },[])
+
+
 
 
   return (
-    <>
+      <div>
+        {
+          posts.map(post => (<div>{post.userId}{post.title}</div>))
+        }
+      </div>
+  );
+};
 
-    </>
-  )
-}
+export default App;
 
-export default App
+
