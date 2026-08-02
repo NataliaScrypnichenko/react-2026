03226ddbd11f5c11213ea74014bc662cce84fbb5
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-08-02 22:42:08 +0300

    I finished the task. #x7sQQ3cJ3

--- src/components/post-component/PostComponent.tsx
+++ src/components/post-component/PostComponent.tsx
@@ -1,13 +1,12 @@
 import type {FC} from "react";
 import type {IPost} from "../../models/IPost.ts";
-
+import './PostComponent.css'
 
 type PostPropType = {
     item:IPost
-}
+};
 
 const PostComponent:FC<PostPropType> = ({item}:PostPropType) => {
-
 
     return (
         <div>
@@ -16,7 +15,6 @@ const PostComponent:FC<PostPropType> = ({item}:PostPropType) => {
                    <h1 className={'post_h1'}>{item.userId} {'--'} {item.id}</h1>
                    <p className={'post_p1'}>{item.title}</p>
                    <p className={'post_p2'}>{item.body}</p>
-
                </div>
             }
         </div>
