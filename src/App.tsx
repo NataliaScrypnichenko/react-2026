5bf7cc4b8f0bb9355e24cb5d53acdf8ae9a6d4b2
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-08-05 21:58:26 +0300

    I Created active pages-components in the App.tsx  #UHbARYeyHp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
 import CommentsComponent from "./components/comment-component/comments/CommentsComponent.tsx";
-// import PostsComponent from "./components/posts-component/Posts/PostsComponent.tsx";
-// import TodosComponent from "./components/todos-component/Todos/TodosComponent.tsx";
+import PostsComponent from "./components/posts-component/Posts/PostsComponent.tsx";
+import TodosComponent from "./components/todos-component/Todos/TodosComponent.tsx";
 
 function App() {
 
 
   return (
     <>
-     {/*<TodosComponent/>*/}
-     {/*   <PostsComponent/>*/}
+     <TodosComponent/>
+      <PostsComponent/>
       <CommentsComponent/>
     </>
   )
