a45f5ed4c0cfa7f3130d76af7b5f9234009306b7
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-08-08 23:18:30 +0300

    lesson3/Router demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const routers=createBrowserRouter([
+    {path:'/', }
+])
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <RouterProvider router={routers} />
 
 )
