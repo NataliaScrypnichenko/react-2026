bc1fec854f3b56a185790d59b944440fa662e157
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-09-01 22:18:51 +0300

    forms-uncontrolled=1

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,14 @@
+
+
+const FormComponent = () => {
+    return (
+        <div>
+             <form>
+                 <input name="username" type="text"  />
+                 <input name="password" type="text" />
+             </form>
+        </div>
+    );
+};
+
+export default FormComponent;
