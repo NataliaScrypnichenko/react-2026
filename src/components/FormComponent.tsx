71462311cc98bdbd3b9ccfe930f7341db3c8cdf3
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-09-03 23:08:45 +0300

    useForm-"react-hook-form": "^7.51.3"

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,11 +1,24 @@
+import {useForm} from "react-hook-form";
 
 
+interface IFormProps{
+    name: string,
+    password: string,
+    age: number,
+
+}
+//все що є в формі =воно є стрінгою
 const FormComponent = () => {
+   const useForm1 = useForm<IFormProps>();
+  console.log(useForm1);//повертає об'єк і там видно багото способів
+
     return (
         <div>
              <form>
-                 <input name="username" type="text"  />
-                 <input name="password" type="text" />
+                 <input name={"username"} type="text" />
+                 <input name={"password"} type="text" />
+                 <input name={"age"} type="number" />
+                 <button>spend</button>
              </form>
         </div>
     );
