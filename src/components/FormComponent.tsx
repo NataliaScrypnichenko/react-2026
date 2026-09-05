63445eb6b9d7cc0900a00bd9f4226bf3db1a4b53
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-09-05 20:56:19 +0300

    useForm-"react-hook-form": "^7.51.3"

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -2,22 +2,35 @@ import {useForm} from "react-hook-form";
 
 
 interface IFormProps{
-    name: string,
+    username: string,
     password: string,
     age: number,
 
 }
 //все що є в формі =воно є стрінгою
 const FormComponent = () => {
-   const useForm1 = useForm<IFormProps>();
-  console.log(useForm1);//повертає об'єк і там видно багото способів
+  //  const useForm1 = useForm<IFormProps>();
+  // console.log(useForm1);//повертає об'єк і там видно багото способів методів тобто розкрити його
+  //   // і побачити що з ним можливо робити
+
+
+    //деструктиризуємо форму- handleSubmit це обробник він ставиться на onSubmit, але він handleSubmit є обгортка
+    // і в себе приймає колбек функцію ()=>{}
+    //register-привязується до інпуту в якому він знаходиться ту інформацію яку ми визначемо
+    const {handleSubmit, register} = useForm<IFormProps>();
+    console.log(register)
+    //так ми привязуемо дані до інпута
+    const customHandle=(formDataProps:IFormProps) =>{
+        console.log(formDataProps)
+    }
+
 
     return (
         <div>
-             <form>
-                 <input name={"username"} type="text" />
-                 <input name={"password"} type="text" />
-                 <input name={"age"} type="number" />
+             <form onSubmit={handleSubmit(customHandle)}>
+                 <input  type="text" {...register('username')} />
+                 <input  type="text" {...register('password')}/>
+                 <input type="number" {...register('age')} />
                  <button>spend</button>
              </form>
         </div>
