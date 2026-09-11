cd57b229cb22459367492eaeb4b7e0a5e19a885a
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-09-11 17:37:30 +0300

    base-validation

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
 тут бібліотеки
+useForm = потрібно валідувати і є 2 варіанти
+1 але він не дужеихороший,тому що пуста фарма є звалідована і її вирішують додаткові бібліотеки які всі їх використовують,хоча можна розтавити кастилі і вони будуть працювати
+formState:{errors, isValid}/errors=помилки які будуть виникати і їх повідомлення\\isValid= буде показувати форма валідна чи ні,Valid=форма є валідна коли кожний її інпут відповідає певній умові перевірці, а коли хоч один не відповідає то форма не валідна
 
+Валідовати об'єкт можемо через другий аргумент в функції register {...register('username',{})}
+<input  type="text" {...register('username',{
+     required:true,
+     pattern:{
+        value:/\w+/,
+        message:'wrong name'
+     }//це ми прописуємо умову , якщо щось не так записано видіється сообщеніе 'wrong name',його можна замінити на 
+     
+})} />
 
+<form onSubmit={handleSubmit(customHandle)}>
+                 <input  type="text" {...register('username',{
+                     required:true,
+                     // pattern:{
+                     //     value:/\w+/,
+                     //     message:'wrong name'
+                     // }
+                     minLength:{value:1, message:'wrong name'}
+                 }  )} />
+                 <input  type="text" {...register('password',
+                     {
+                         required:true,
+                         minLength:{value:3,message:'pass too short'},
+                         maxLength:{value:6, message:'pass too long'}
+                     })}/>
+                 <input type="number" {...register('age',
+                     {
+                         required:true,
+                         valueAsNumber:true, // це означає що автоматично конвертується в числове значення
+                         min:{value:1, message:'age too small'},
+                         max:{value:117, message:'age too big'}
+
+                     })} />
+                  <button disabled={!isValid}>spend</button>// для того щоб активовувати чи дезеактивовувати кнопку
+             </form>
+тепер це означає що ту інформацію яку будем водитит інформацію тобто перевірятися на всьому єтапі
+
+const {
+     handleSubmit,
+     register,
+     formState:{},
+} = useForm<IFormProps>({
+         mode:'аll'//стрінгове значення режим перевірки коли будуть валідуватися,onChange-коли будемо водити нове                   значення
+                //onTouched=коли будем фокусуватися на форму.\onSubmit=коли будем відправляти форму
+                / \onBlur= коли будемо втрачатифокус із інпута форми./all-це  всі тригери
+});
+ це означає що в будь-якому варіанті будем валідувати форму
--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,6 +1,7 @@
 import {useForm} from "react-hook-form";
 
 
+
 interface IFormProps{
     username: string,
     password: string,
@@ -9,32 +10,62 @@ interface IFormProps{
 }
 //все що є в формі =воно є стрінгою
 const FormComponent = () => {
-  //  const useForm1 = useForm<IFormProps>();
-  // console.log(useForm1);//повертає об'єк і там видно багото способів методів тобто розкрити його
-  //   // і побачити що з ним можливо робити
 
+    const {
+        handleSubmit,
+        register,
+        formState:{errors,isValid}
+    } = useForm<IFormProps>({
+        mode:'all'
+    });
 
-    //деструктиризуємо форму- handleSubmit це обробник він ставиться на onSubmit, але він handleSubmit є обгортка
-    // і в себе приймає колбек функцію ()=>{}
-    //register-привязується до інпуту в якому він знаходиться ту інформацію яку ми визначемо
-    const {handleSubmit, register} = useForm<IFormProps>();
-    console.log(register)
     //так ми привязуемо дані до інпута
     const customHandle=(formDataProps:IFormProps) =>{
-        console.log(formDataProps)
+        console.log(formDataProps)// сприймаємо як якусь дію
     }
 
 
     return (
         <div>
              <form onSubmit={handleSubmit(customHandle)}>
-                 <input  type="text" {...register('username')} />
-                 <input  type="text" {...register('password')}/>
-                 <input type="number" {...register('age')} />
-                 <button>spend</button>
+                <label>
+                    <input  type="text" {...register('username',{
+                     required:{value:true, message:'name is required'},
+                     // pattern:{
+                     //     value:/\w+/,
+                     //     message:'wrong name'
+                     // }
+                     minLength:{value:4, message:'wrong name'}
+                 }  )} />
+
+                    <div>{errors.username && <div>{errors.username.message}</div>}</div>
+                </label>
+                 <label>
+                 <input  type="text" {...register('password',
+                     {
+                         required:true,
+                         minLength:{value:3,message:'pass too short'},
+                         maxLength:{value:12, message:'pass too long'}
+                     })}/>
+                     <div>{errors.password && <div>{errors.password.message}</div>}</div>
+                 </label>
+                 <label>
+                 <input type="number" {...register('age',
+                     {
+                         required:true,
+                         valueAsNumber:true, // це означає що автоматично конвертується в числове значення
+                         min:{value:1, message:'age too small'},
+                         max:{value:117, message:'age too big'}
+
+                     })} />
+                     <div>{errors.age && <div>{errors.age.message}</div>}</div>
+                 </label>
+                 <button disabled={!isValid}>spend</button>
              </form>
         </div>
     );
 };
-
+//виводимо меседжес під цими інпутом
+//воно буде викидати на новий рядок, то краще оформити через блоки в css дісплей блоком, щоб такого не було
+// то потрібні другі бібліотеки
 export default FormComponent;
