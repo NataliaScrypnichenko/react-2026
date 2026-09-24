5bb8af5b365ada3c1574ac1b3d33e26539140a76
NataliaScrypnichenko <cornanatalia604@gmail.com>
2026-09-24 21:22:25 +0300

    I finished

--- src/component/product/ProductComponent.tsx
+++ src/component/product/ProductComponent.tsx
@@ -1,14 +1,21 @@
 import type {FC} from "react";
 import type {IProduct} from "../../model/IProduct.ts";
+import './ProductComponent.css'
 
 export type ProductPropsType={
     product: IProduct;
-}
+};
 
 const ProductComponent:FC<ProductPropsType> = ({product}) => {
     return (
-        <div>
-            {product.title}
+        <div className="product">
+            <img src={product.thumbnail} alt={product.title}/>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>Category: {product.category}</p>
+            <p className="price">${product.price}</p>
+            <p>Rating: {product.rating}</p>
+            <p>Stock: {product.stock}</p>
         </div>
     );
 };
--- src/component/product/ProductsComponent.tsx
+++ src/component/product/ProductsComponent.tsx
@@ -1,17 +1,25 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {loadAuthProduct, refresh} from "../../service/api.servise.ts";
+import type {IProduct} from "../../model/IProduct.ts";
+import ProductComponent from "./ProductComponent.tsx";
 
 const ProductsComponent = () => {
+    const [products, setProducts] = useState<IProduct[]>([])
+
     useEffect(() => {
         loadAuthProduct()
             .then(products=> {
                 console.log(products);
+                setProducts(products);
             }).catch(reason => {//якщо помилка тобто не працє токен то виконується refresh
             console.log(reason);
             refresh()
                 .then(()=> {
                     loadAuthProduct()//знову викликає продукти
-                        .then(value => console.log(value))
+                        .then(value => {
+                            console.log(value)
+                            setProducts(value)
+                        })
                 });
         });
 
@@ -19,7 +27,9 @@ const ProductsComponent = () => {
 
     return (
         <div>
-
+            {
+                products.map((product) => (<ProductComponent product={product} key={product.id} />))
+            }
         </div>
     );
 };
